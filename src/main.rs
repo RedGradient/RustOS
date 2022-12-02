083c5928050221e25b3e5fd749bd48b4e3a55c3f
redgradient <neutral1shade@yahoo.com>
2022-12-02 06:08:50 +0400

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@
 #![no_main]
 
 mod vga_buffer;
-
-use core::arch::x86_64::{_mm_fmadd_sd, _MM_FROUND_TO_POS_INF};
 use core::panic::PanicInfo;
-use core::fmt::Write;
 
-
-
-// static HELLO: &[u8] = b"Hello, World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
